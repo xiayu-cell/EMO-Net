9d4d6a3b8735d4019e5c04fb621f9e42868a1983
xiayu-cell <73735019+xiayu-cell@users.noreply.github.com>
2023-07-10 21:10:05 +0800

    Update attention.py

--- model/attention.py
+++ model/attention.py
@@ -1,11 +1,3 @@
-'''
-Author: Rain 1254895072@qq,com
-Date: 2023-06-16 21:51:13
-LastEditors: Rain 1254895072@qq,com
-LastEditTime: 2023-06-16 21:52:04
-FilePath: \EMO-Net\model\attention.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import time
 from torch import nn, Tensor
 from torch.nn import functional as F
